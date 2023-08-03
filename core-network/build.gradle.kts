5554c8a382e2f190338bad517857c7b021bc9073
koreatlwls <koreatlwls@gmail.com>
2023-08-03 14:06:34 +0900

    Chore : BuildConfig 수정

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -19,9 +19,13 @@ android {
         buildConfigField("String", "BACK_URL", getLocalProperties("BACK_URL"))
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
