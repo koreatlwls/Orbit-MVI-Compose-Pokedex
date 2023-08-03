cdef99cf698a024cb7e673f8b4e5ec04ae27c021
koreatlwls <koreatlwls@gmail.com>
2023-08-03 17:51:51 +0900

    Fix : endpoint

--- core-network/src/main/kotlin/koreatlwls/pokedex/core/service/PokemonService.kt
+++ core-network/src/main/kotlin/koreatlwls/pokedex/core/service/PokemonService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface PokemonService {
 
-    @GET("/pokemon")
+    @GET("api/v2/pokemon/")
     suspend fun getPokemons(
         @Query("offset") offset: Int = 0,
         @Query("limit") limit: Int = 20,
