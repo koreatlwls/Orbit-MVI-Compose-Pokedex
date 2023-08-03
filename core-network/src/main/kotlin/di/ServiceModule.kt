9f7f3e6926dc7ece6bec170217ef1300728d1f6c
koreatlwls <koreatlwls@gmail.com>
2023-08-03 14:31:54 +0900

    Feat : PokemonClient 추가

--- core-network/src/main/kotlin/di/ServiceModule.kt
+++ core-network/src/main/kotlin/di/ServiceModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import service.PokemonClient
 import service.PokemonService
 import javax.inject.Singleton
 
@@ -14,7 +15,12 @@ object ServiceModule {
 
     @Provides
     @Singleton
-    fun providePokemonSercie(retrofit: Retrofit): PokemonService =
+    fun providePokemonService(retrofit: Retrofit): PokemonService =
         retrofit.create(PokemonService::class.java)
+
+    @Provides
+    @Singleton
+    fun providePokemonClient(pokemonService: PokemonService) =
+        PokemonClient(pokemonService)
 
 }
