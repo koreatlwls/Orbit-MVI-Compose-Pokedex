822cfec259e4f12778eee04220dd311b7dbea526
koreatlwls <koreatlwls@gmail.com>
2023-08-07 18:03:34 +0900

    Feat : 랜덤 컬러 값 extension 구현 및 컬러 값 추가

--- app/src/main/kotlin/koreatlwls/pokedex/ui/theme/Color.kt
+++ app/src/main/kotlin/koreatlwls/pokedex/ui/theme/Color.kt
@@ -6,16 +6,19 @@ import androidx.compose.ui.graphics.Color
 val PdsBlack = Color(0xFF000000)
 val PdsWhite = Color(0xFFFFFFFF)
 val PdsLightWhite = Color(0x80FFFFFF)
+val PdsLightGray = Color(0xB3CCCBCB)
 
 enum class PdsColor {
     PDS_BLACK,
     PDS_WHITE,
+    PDS_LIGHT_GRAY,
     PDS_LIGHT_WHITE;
 
     @Composable
     fun getColor() = when (this) {
         PDS_BLACK -> PdsTheme.colors.black
         PDS_WHITE -> PdsTheme.colors.white
+        PDS_LIGHT_GRAY -> PdsTheme.colors.lightGray
         PDS_LIGHT_WHITE -> PdsTheme.colors.lightWhite
     }
 }
@@ -24,4 +27,5 @@ data class PdsColors(
     val black: Color = PdsBlack,
     val white: Color = PdsWhite,
     val lightWhite: Color = PdsLightWhite,
+    val lightGray: Color = PdsLightGray,
 )
